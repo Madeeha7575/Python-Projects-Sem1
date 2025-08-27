32a232255ac8634379fce8f19683bcc241e2a240
Madeeha Khan <madeeha.khan0008@gmail.com>
2025-08-27 17:06:46 +0530

    Update #Marks sort and avg.py

--- #Marks sort and avg.py
+++ #Marks sort and avg.py
@@ -1,6 +1,7 @@
 #Marks sort and avg
-"""marks = list(map(float,input("Enter marks seperated by spaces:").split()))
+marks = list(map(float,input("Enter marks seperated by spaces:").split()))
 marks.sort()
 avg = sum(marks)/len(marks)
 print("Sorted marks:",marks)
-print("Avg:",avg)"""
+
+print("Avg:",avg)
